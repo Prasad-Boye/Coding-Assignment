ef0eafb07cccbb21d382bfafaeaaf698e466dad9
Prasad Boye <boyaprasad05@outlook.com>
2022-08-08 11:59:31 +0530

    revision 2 - commit 2

--- analyze_quotes.py
+++ analyze_quotes.py
@@ -6,7 +6,7 @@ cursor = connection.cursor()
 
 ### Query 1 Get total number of quotations on the website
 
-def get_total_no_of_quotes():
+def print_total_no_of_quotes():
     total_quotes = cursor.execute("SELECT count(*) FROM quotes")
     for row in total_quotes:
         print("Total no. of quotations on the website :",row[0])
@@ -17,7 +17,7 @@ def get_total_no_of_quotes():
 
 
 
-def get_no_of_quotes_by_author():
+def print_no_of_quotes_by_author():
     author = "Albert Einstein"
 
     cursor.execute("SELECT count(*)\
@@ -38,7 +38,7 @@ def get_no_of_quotes_by_author():
 
 ### Query 3 Minimum, Maximum, and Average no. of tags on the quotations
 
-def get_max_min_avg_of_tags():
+def print_max_min_avg_of_tags():
     cursor.execute("SELECT MIN(no_of_tags),MAX(no_of_tags),AVG(no_of_tags)\
         FROM (SELECT count(tag_id) as no_of_tags,quotes.id as quote  \
             FROM quotes LEFT JOIN \
@@ -71,7 +71,7 @@ def find_top_authors(limit_value):
     return top_authors
 
 
-def get_top_authors():
+def print_top_authors():
     top_number = 5
     top_authors = find_top_authors(top_number)
     print("Top {} Authors and no. of quotes:".format(top_number))
@@ -81,10 +81,10 @@ def get_top_authors():
 
 
 def get_all_results():
-    get_total_no_of_quotes()
-    get_no_of_quotes_by_author()
-    get_max_min_avg_of_tags()
-    get_top_authors()
+    print_total_no_of_quotes()
+    print_no_of_quotes_by_author()
+    print_max_min_avg_of_tags()
+    print_top_authors()
 
 
 get_all_results()
